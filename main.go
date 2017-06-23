695ebb8d432660361e6377ab19c885dd054d1aa7
Christoph <ignatz@gmail.com>
2017-06-23 09:39:25 -0400

    Added static factory Goname method to main

--- main.go
+++ main.go
@@ -2,6 +2,32 @@ package main
 
 import "fmt"
 
+type NameGenerator struct {
+	pre_syllables 	[]Syllable
+	mid_syllables	[]Syllable
+	sur_syllables  	[]Syllable
+}
+
+func Goname(dialectMap map[string]Syllable) NameGenerator {
+	var first []Syllable
+	var mid []Syllable
+	var last []Syllable
+
+	for _, v := range dialectMap {
+		if v.isPrefix {
+			first = append(first, v)
+		} else if v.isSuffix {
+			last = append(last, v)
+		} else {
+			mid = append(mid, v)
+		}
+	}
+	return NameGenerator{
+		pre_syllables: first,
+		mid_syllables: mid,
+		sur_syllables: last}
+}
+
 //Generate random name.
 func Generate(number int) string {
 	return "notrandom"
